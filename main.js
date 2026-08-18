646511d51439a4e2d6fa5ab1b9681136bd853b0b
Japa2207 <eduardo.otramario.passos@escola.pr.gov.br>
2026-08-18 12:16:40 -0300

    .

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const caixaPrincipal = document.querySelector(".caixa-principal")
 const caixaPerguntas = document.querySelector(".caixa-perguntas")
 const caixaAlternativas = document.querySelector(".caixa-alternativas")
 const caixaResultados = document.querySelector(".caixa-resultados")
-const textoResultados = document.querySelector(".texto-resultado")
+const textoResultado = document.querySelector(".texto-resultado")
 
 
 
