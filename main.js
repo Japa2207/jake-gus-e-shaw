2a5708345e2968911ada97ca042911956ea35baf
Japa2207 <eduardo.otramario.passos@escola.pr.gov.br>
2026-09-15 12:12:24 -0300

    ,op-lp,

--- main.js
+++ main.js
@@ -139,7 +139,10 @@ for  (const alternativa of perguntaAtual.alternativas){
             }
         
 function aleatorio(lista){
-    cost posicao = Math.random()*lista.length
+    cost posicao = math.floor (Math.random()*lista.length)
+    console.log(posicao)
+    return(lista[posicao])
+
 }
         
   mostraPergunta()  
